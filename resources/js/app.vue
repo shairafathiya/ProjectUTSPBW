28437bc176d05195e8cced857d6e1f5625f11b98
R. Muhammad Arif Rijal <shairafathiya@gmail.com>
2025-04-12 11:46:39 +0700

    backup

---
+++ resources/js/app.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello Vue + Laravel 👋</h1>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel Vite Example</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    
+</head>
+<body>
+    <div id="app"> HELLO</div>
+</body>
+</html>
